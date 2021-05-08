9d4e0bf4faa581ba8a4a7804c3ae4f38359738d9
swc9803 <swc9803@gmail.com>
2021-05-08 22:48:27 +0900

    asd

--- src/components/appcomponents/Navbar.vue
+++ src/components/appcomponents/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-sm navbar-light bg-light">
-    <router-link :to="{ name: 'Home'}">
+    <router-link class="fa fa-home" :to="{ name: 'Home'}">
       Home
     </router-link>
     <ul class="navbar-nav mr-auto">
@@ -30,15 +30,15 @@
         </router-link>
       </li>
     </ul>
-    <div>
-      <button class="btn btn-primary">
-        <router-link class="Login" :to="{ name: 'Login'}">
-          로그인
-        </router-link>
-      </button>
       <div>
+        <button class="btn btn-primary">
+          <router-link id="Login" class="fa fa-sign-in" :to="{ name: 'Login'}">
+            로그인
+          </router-link>
+        </button>
+        <div>
         <button class="btn btn-secondary" style="width:100px">
-          <router-link class="Login" :to="{ name: 'SignUp'}">
+          <router-link id="Login" :to="{ name: 'SignUp'}">
             회원가입
           </router-link>
         </button>
@@ -59,7 +59,7 @@ export default {
   font-size: 20px;
   margin-left: 20px;
 }
-.Login {
+#Login {
   color: white;
 }
 i {
