d3cc1a8c7c35d93380e595bc7a7ec0d2e51545ad
swc9803 <swc9803@gmail.com>
2021-05-09 21:32:43 +0900

    asdsad

--- src/main.js
+++ src/main.js
@@ -3,17 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import firebase from 'firebase'
-import 'firebase/firestore'
 
 createApp(App).use(store).use(router).mount('#app')
 
 const firebaseConfig = {
   apiKey: 'AIzaSyA8qqqeszhsXlKi_kIZTKlCXnfLo_lBWaw',
   authDomain: 'rhdyd-a7e3b.firebaseapp.com',
+  databaseURL: 'https://rhdyd-a7e3b-default-rtdb.firebaseio.com',
   projectId: 'rhdyd-a7e3b',
   storageBucket: 'rhdyd-a7e3b.appspot.com',
   messagingSenderId: '316902768690',
   appId: '1:316902768690:web:06bc8c4b62fe8150697c2b',
   measurementId: 'G-6TTHN5HP14'
 }
+
 firebase.initializeApp(firebaseConfig)
