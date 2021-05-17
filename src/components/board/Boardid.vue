edab232f8559ed0b0a699e76e1e8b35b271bbe19
swc9803 <swc9803@gmail.com>
2021-05-17 13:08:17 +0900

    dd

--- src/components/board/Boardid.vue
+++ src/components/board/Boardid.vue
@@ -1,28 +1,15 @@
 <template>
-<h1 class="ml-5">문서 수정하기</h1>
-<div class="form">
-  <form>
-    <label>Title</label>
-      <div class="row">
-        <div class="col-6">
-          <input type="text" class="form-control" style="width: 300px" v-model="form.title" required>
-        </div>
-        <div class="col-12">
-          <div class="form-group">
-            <label>Content</label>
-            <textarea class="form-control" cols="30" rows="10" style="width: 70%" v-model="form.content" required></textarea>
-          </div>
-        </div>
-      </div>
-  </form>
-<button class="btn btn-primary" @click="updateform">Update</button>
-<button class="btn btn-outline-dark ml-2" @click="moveToBoard">Cancel</button>
+  <div>
+    <h2 >Board</h2> <p style="float: right">총 {{ forms.length }}개의 게시글이 있습니다.</p>
+
+    글 작성하기
+
 </div>
 </template>
-
 <script>
 import { db } from '@/fdb'
 import 'firebase/firebase-firestore'
+// import { useRouter } from 'vue-router'
 
 export default {
   data () {
@@ -34,32 +21,18 @@ export default {
       }
     }
   },
-  methods: {
-    moveToBoard () {
-      this.$router.push({
-        name: 'Board'
-      })
-    },
-    async updateform () {
-      await db.collection('forms').doc('HWecZ5AgYmQFB5ntb88R').set( // this.form
-        {
-          title: this.form.title, content: this.form.content
-        }
-      ).then(() => {
-        alert('Saved!')
-        this.$router.push({
-          name: 'Board'
-        })
-      })
-        .catch((error) => {
-          alert('Error : ' + error.message)
-        })
+  async created () {
+    const sn = db.collection('forms').doc('form.id')
+    const doc = await sn.get()
+    if (!doc.exists) {
+      console.log('No such document!')
+    } else {
+      console.log('Document data:', doc.data())
     }
   }
 }
 </script>
-<style scoped>
-  .form {
-    margin-left: 60px;
-  }
+
+<style>
+
 </style>
