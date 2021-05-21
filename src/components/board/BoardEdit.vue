ff9ad66140976c137a1768a399ac545e45079126
swc9803 <swc9803@gmail.com>
2021-05-21 09:35:55 +0900

    update

--- src/components/board/BoardEdit.vue
+++ src/components/board/BoardEdit.vue
@@ -1,30 +1,30 @@
 <template>
-<h1 class="ml-5">글 수정하기</h1>
-<hr>
-<div class="form">
-  <form>
-    <label>Title</label>
-      <div class="row">
-        <div class="col-6">
-          <input type="text" class="form-control" style="width: 300px" v-model="form.title" required>
-        </div>
-        <div class="col-12">
-          <div class="form-group">
-            <label>Content</label>
-            <textarea class="form-control" cols="30" rows="10" style="width: 70%" v-model="form.content" required></textarea>
+  <h1 class="ml-5">글 수정하기</h1>
+  <hr>
+  <div class="form">
+    <form>
+      <label>Title</label>
+        <div class="row">
+          <div class="col-6">
+            <input type="text" class="form-control" style="width: 300px" v-model="form.title" required>
+          </div>
+          <div class="col-12">
+            <div class="form-group">
+              <label>Content</label>
+              <textarea class="form-control" cols="30" rows="10" style="width: 70%" v-model="form.content" required></textarea>
+            </div>
           </div>
         </div>
-      </div>
-  </form>
-<button class="btn btn-primary" @click="updateform">저장</button>
-<button class="btn btn-outline-dark ml-2" @click="moveToBoard">취소</button>
-</div>
+    </form>
+  <button class="btn btn-primary" @click="updateform">저장</button>
+  <button class="btn btn-outline-dark ml-2" @click="moveToBoard">취소</button>
+  </div>
 </template>
 
 <script>
 import { db } from '@/fdb'
-import { useRouter, useRoute } from 'vue-router'
-import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import firebase from 'firebase'
 
 export default {
   data () {
@@ -32,43 +32,41 @@ export default {
       form: {
         title: '',
         content: '',
-        updatedAt: ''
+        createdAt: '',
+        uid: ''
       }
     }
   },
-  props: {
-    title: String,
-    content: String
-  },
-  setup (props) {
+  setup () {
     const router = useRouter()
-    const route = useRoute()
-
-    const title = ref('')
-    const content = ref('')
-    const updatedAt = new Date()
 
     const moveToBoard = () => {
       router.push({
         name: 'Board'
       })
     }
-
-    const updateform = async () => {
-      await db.collection('forms').doc(route.params.id).set(
-        {
-          title, content, updatedAt
-        }
-      ).then(() => {
-        alert('업데이트 완료!')
-        router.push({
-          name: 'Board'
-        })
-      })
-    }
     return {
-      moveToBoard,
-      updateform
+      moveToBoard
+    }
+  },
+  methods: {
+    async updateform () {
+      const uid = firebase.auth().currentUser.uid
+      const createdAt = new Date()
+      if (uid === '') {
+        alert('로그인을 해주세요')
+      } else {
+        await db.collection('forms').doc(this.$route.params.id).set(
+          {
+            title: this.form.title, content: this.form.content, createdAt, uid
+          }
+        ).then(() => {
+          alert('수정 완료!')
+          this.$router.push({
+            name: 'Board'
+          })
+        })
+      }
     }
   }
 }
