2533748b0aa406402e697008cb350094d84d8fdd
swc9803 <swc9803@gmail.com>
2021-05-25 06:26:15 +0900

    modal

--- src/components/board/Boardid.vue
+++ src/components/board/Boardid.vue
@@ -1,5 +1,22 @@
 <template>
-  <Boardheader />
+  <form class="form">
+    <div class="title card mt-4">
+      <div class="ml-2">{{ form.title }} <em v-if="form.createdAt !== form.updatedAt" class="badge bg-success" style="font-size: 17px">수정됨</em>
+        <span class="badge badge-light mr-4 mt-2" style="float: right">작성자 : {{ form.name }}</span>
+        <p class="date mr-3">작성일 : {{ form.createdAt }}</p>
+        <p v-if="form.createdAt !== form.updatedAt" class="date mr-3">수정일 : {{ form.updatedAt }}</p>
+      </div>
+    </div>
+    <div class="card mt-4" style="height: 400px">
+      <div class="ml-2">
+        {{ form.content }}
+      </div>
+    </div>
+    <div v-if="user.uid === form.uid" class="btn m-2">
+      <router-link :to="{ name: 'BoardEdit'}"><button class="btn btn-secondary mr-0">수정</button></router-link>
+      <button @click.prevent="openModal" class="btn btn-danger mr-3">삭제</button>
+    </div>
+  </form>
   <div class="btn">
     <Modal
       v-if="showModal"
@@ -14,8 +31,9 @@
 </template>
 
 <script>
-import Boardheader from '@/components/board/Boardheader'
 import { useRouter, useRoute } from 'vue-router'
+import 'firebase/firebase-firestore'
+import firebase from 'firebase'
 import { db } from '@/fdb'
 import Modal from '@/components/Modal'
 import { ref } from 'vue'
@@ -24,6 +42,13 @@ import Comment from '@/components/Comment'
 export default {
   data () {
     return {
+      forms: [],
+      form: {
+        title: '',
+        content: '',
+        createdAt: '',
+        updatedAt: ''
+      },
       user: ''
     }
   },
@@ -66,14 +91,44 @@ export default {
     }
   },
   components: {
-    Boardheader,
     Modal,
     Comment
+  },
+  async created () {
+    const route = useRoute()
+    const forminfo = db.collection('forms').doc(route.params.id)
+    const doc = await forminfo.get()
+    this.form.uid = doc.data().uid
+    this.form.name = doc.data().name
+    this.form.title = doc.data().title
+    this.form.content = doc.data().content
+    this.form.createdAt = doc.data().createdAt
+    this.form.updatedAt = doc.data().updatedAt
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.user = user
+      } else {
+        this.user = ''
+      }
+    })
   }
 }
 </script>
 
 <style scoped>
+  .date {
+    text-align: right;
+    font-size: 15px;
+  }
+  .title {
+    background: lightgray;
+  }
+  .form {
+    width: 70%;
+    margin: 0 auto;
+    float: none;
+    margin-bottom: 10px;
+  }
   .btn {
     float: right;
     margin-right: 10%;
