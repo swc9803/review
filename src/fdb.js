cfd65a56eaf0a390004f36992152465243ee1a2d
swc9803 <swc9803@gmail.com>
2021-06-01 11:27:17 +0900

    5

--- src/fdb.js
+++ src/fdb.js
@@ -15,8 +15,10 @@ const firebaseApp = firebase.initializeApp(firebaseConfig)
 
 const db = firebaseApp.firestore()
 const auth = firebase.auth()
+const storage = firebase.storage()
 
 export {
   db,
-  auth
+  auth,
+  storage
 }
